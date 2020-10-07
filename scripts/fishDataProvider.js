e02595ab204a1b4f864cdb3f6cf9fca94cba73ba
Matthew Machurek <machurek@Matthews-MBP.attlocal.net>
2020-10-07 13:58:35 -0500

    getting site ready for js

---
+++ scripts/fishDataProvider.js
@@ -0,0 +1,10 @@
+const fishCollection = [
+    {
+        name: "Chevy",
+        image: "",
+        species: "Neon Tetra",
+        size: "1.5",
+        diet: "Brine Shrimp, Daphnia & Bloodworms",
+        harvested: "Amazon River Basin, Brazil"
+    }
+]
