018c0dbacbe079381cf2c6c5fc8810367d363e2b
Matthew Machurek <mrjm2005@yahoo.com>
2020-10-07 15:37:32 -0500

    Added properties to fish js

---
+++ scripts/fish.js
@@ -0,0 +1,16 @@
+
+
+export const Fish = (fish) => {
+    return `
+        <div class="aquarium__fish--each">
+            <h3 style="font-size: 20px;">${fish.name}</h3>
+            <p><img src="${fish.image}"></p>
+            <p>Species: ${fish.species}</p>
+            <p>Size: ${fish.size}</p>
+            <p>Diet: ${fish.diet}</p>
+            <p>Harvested Location: ${fish.harvested}</p>
+        </div>
+    `
+}
+
+console.log(fish)
