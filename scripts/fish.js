ffd6622d3dfd716abfc50ab30077f1262a815875
Matthew Machurek <mrjm2005@yahoo.com>
2020-10-08 16:22:04 -0500

    Found th elost code in github and fixed

--- scripts/fish.js
+++ scripts/fish.js
@@ -2,11 +2,13 @@
 
 export const Fish = (fishEach) => {
     return `
+        <div class="aquarium__fish--each">
             <h3 style="font-size: 20px;">${fishEach.name}</h3>
             <p><img src="${fishEach.image}"></p>
             <p>Species: ${fishEach.species}</p>
             <p>Size: ${fishEach.size}</p>
             <p>Diet: ${fishEach.diet}</p>
             <p>Harvested Location: ${fishEach.harvested}</p>
+        </div>
     `
 }
